335fd08455cd8f07c6368e4a729a7bd648bb3047
Your Name <your.email@example.com>
2025-05-11 13:34:20 +0530

    Changes made

--- FacebookFirst/Search.java
+++ FacebookFirst/Search.java
@@ -8,15 +8,15 @@ public static void Search(String[] args) {
     for (int i = 0; i < n; i++) {
         a[i] = sc.nextInt();
     }
-    int[] b = new int[m];
-    for (int i = 0; i < m; i++) {
+    int[] b = new int[s];
+    for (int i = 0; i < s; i++) {
         b[i] = sc.nextInt();
     }
-    int[] c = new int[n + m];
+    int[] c = new int[n + s];
     for (int i = 0; i < n; i++) {
         c[i] = a[i];
     }
-    for (int i = n; i < n + m; i++) {
+    for (int i = n; i < n + s; i++) {
         c[i] = b[i - n];
     }
     Arrays.sort(c);
