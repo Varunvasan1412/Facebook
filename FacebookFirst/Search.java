40d26e0b2065cae7d310fc53b5e6ee83cfd4fbd0
Your Name <your.email@example.com>
2025-05-11 12:57:46 +0530

    Search file

---
+++ FacebookFirst/Search.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public static void Search(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
+        a[i] = sc.nextInt();
+    }
+    int[] b = new int[m];
+    for (int i = 0; i < m; i++) {
+        b[i] = sc.nextInt();
+    }
+    int[] c = new int[n + m];
+    for (int i = 0; i < n; i++) {
+        c[i] = a[i];
+    }
+    for (int i = n; i < n + m; i++) {
+        c[i] = b[i - n];
+    }
+    Arrays.sort(c);
